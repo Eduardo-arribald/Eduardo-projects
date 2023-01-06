b5aabfcf54de30afed42feb02ee033838cb82731
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-06 06:20:02 +0000

    Fri, Jan 6, 2023, 6:20 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,9 @@
 
-q = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip()
 
-answers = [42, "Forty Two", "forty-two"]
+yes = [42, "Forty Two", "forty-two"]
 
-if
+if answer in yes:
+    print("Yes")
+elif answer == 50:
+    print("No")
