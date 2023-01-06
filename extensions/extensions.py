5fb5a9576ad09970dd607133a884d3d3b0918726
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-06 20:47:34 +0000

    Fri, Jan 6, 2023, 8:47 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,10 +5,12 @@ file = list(input("File name: "))
 for i in range(len(file)):
     if file[i] == ".":
         file[i] = "/"
+        file = ''.join(file)
+        print(file)
+        break
+    elif "." not in file:
+        print("application/octet-stream")
         break
     else:
         continue
 
-file = ''.join(file)
-
-print(file)
