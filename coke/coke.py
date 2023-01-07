85b286700f3b9500982fdc6967bef8c6139d20b5
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 17:05:28 +0000

    Sat, Jan 7, 2023, 5:05 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,16 +3,17 @@ def main():
     money = 0
     while True:
         x = 50
-        print(f"Amount due: {x}")
+        print(f"Amount due: {x-money}")
         coin = input("Insert Coin: ")
-        check_coins(coin)
-        
-        break
+        money = money + check_coins(coin)
+        if money >= 50:
+            print(f"Change Owerd: {money - x}")
+            break
 
 def check_coins(c):
     coins = ["25", "10", "5"]
     if c in coins:
         c = int(c)
-
+        return c
 
 main()
