6d1823afede80f146f963859c347b8a17d4e0266
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 06:00:50 +0000

    Sat, Jan 7, 2023, 6:00 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,7 +2,12 @@
 def main():
     t = input("What time is it? ")
     t = convert(t)
-    
+    if 7 <= t <=8:
+        print("breakfast time")
+    elif 12 <= t <= 13:
+        print("lunch time")
+    elif 18 <= t <= 19:
+        print("dinner time")
 
 def convert(time):
     n = (''.join(time.split())).split(":")
