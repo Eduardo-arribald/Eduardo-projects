4ada659ab457be680346e84935f99462e4c98b8c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 06:07:19 +0000

    Sat, Jan 7, 2023, 6:07 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,20 +1,31 @@
 
 def main():
     t = input("What time is it? ")
-    t = convert(t)
+    convert_2(t)
     #print(t)
+    """
     if 7 <= t <=8:
         print("breakfast time")
     elif 12 <= t <= 13:
         print("lunch time")
     elif 18 <= t <= 19:
         print("dinner time")
+        """
 
 def convert(time):
     n = (''.join(time.split())).split(":")
     h, m = n[0], n[1]
     new = round((int(h) + (int(m)/60)), 2)
     return new
+
+def convert_2(time):
+    n = time.split()
+    n = ''.join(n[0:2])
+    print(n)
+    #h, m = n[0], n[1]
+    #new = round((int(h) + (int(m)/60)), 2)
+    #return new
+
 
 if __name__ == "__main__":
     main()
