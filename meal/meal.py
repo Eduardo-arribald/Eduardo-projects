3eecbaf7d360ae1cec1fffae5a8c64733953b64a
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 05:55:04 +0000

    Sat, Jan 7, 2023, 5:55 AM +00:00

---
+++ meal/meal.py
@@ -0,0 +1,14 @@
+
+def main():
+    t = input("What time is it? ")
+    convert(t)
+    print(convert(t))
+
+def convert(time):
+    n = (''.join(time.split())).split(":")
+    h, m = n[0], n[1]
+    new = round((int(h) + (int(m)/60)), 1)
+    return new
+
+if __name__ == "__main__":
+    main()
