e0a307edeac330f4c1050b045a21c422412fb7fd
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 05:32:14 +0000

    Sat, Jan 7, 2023, 5:32 AM +00:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,6 +1,12 @@
 
+vars = input("Expression: ").split(" ")
+x, y, z = vars[0], vars[1], vars[2]
+
+print(vars)
+
 def mats():
     while True:
+        vars = input("Expression: ").split(" ")
         x, y, z = input("Expression: ").split(" ")
         x, z = int(x), int(z)
         match y:
@@ -23,7 +29,7 @@ def mats():
                 print(float(x^z))
                 break
 
-mats()
+#mats()
 
 #print(x+z)
 
