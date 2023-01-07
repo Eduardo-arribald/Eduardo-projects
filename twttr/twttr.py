47a491ecb67b1057ca8b8b46f7e79ec06c28daa9
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 17:33:25 +0000

    Sat, Jan 7, 2023, 5:33 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,17 +2,17 @@
 
 def main():
     words = input("Input: ")
-    #delete_vowels(words)
-    print ("Output", delete_vowels(words))
+    delete_vowels(words)
+    #print ("Output", delete_vowels(words))
 
 def delete_vowels(w):
     vowels = ("a", "e", "i", "o", "u")
     w = list(''.join(w.split()))
     print(w)
-    #for l in range(len(w)):
-        #if w[l] in vowels:
+    for l in range(len(w)):
+        if w[l] in vowels:
             #print(w[l])
-            #w.pop(l)
-    return (''.join(w))
+            w.pop(l)
+    #return (''.join(w))
 
 main()
