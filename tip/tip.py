34a5a8ca66869e96191ce1f6a165fb45144f597d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 07:58:44 +0000

    Sat, Jan 7, 2023, 7:58 AM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -2,18 +2,16 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    print(dollars)
-    print(percent)
-    #tip = dollars * percent
-    #print(f"Leave ${tip:.2f}")
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-    d = round(float(d.split("$")), 1)
+    d = round(float(''.join(d.split("$"))), 1)
     return d
 
 def percent_to_float(p):
-    p = round(float(p.split("%"))/100, 2)
+    p = round(float(''.join(p.split("%")))/100, 2)
     return p
 
 main()
