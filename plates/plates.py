5928a16458fbbf38f317880f0dd75c484346aa7c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 19:42:26 +0000

    Sat, Jan 7, 2023, 7:42 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -24,9 +24,9 @@ def is_valid(s):
     else:
         s = list(''.join(s))
         print(s)
-        if 2 <= s <= 6: #This checks the lenght of the word.
+        if 2 <= len(s) <= 6: #This checks the lenght of the word.
             if s[0].lower() and s[1].lower() in alpha: #Check if the first two are letters.
-                if 
+                if
                     for i in range(len(s)):
                         if s[i] not in punctuations:
 
