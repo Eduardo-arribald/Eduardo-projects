6bc87db984993718d343c838a413fc5d04171a3a
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 19:29:52 +0000

    Sat, Jan 7, 2023, 7:29 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -15,12 +15,13 @@ def is_valid(s):
     #I take off the spaces before and after the text.
     s = s.split() #A list of the word given.
     if len(s) >= 2: #This corrects if the user gives me more than one word
+        print("Two much words")
         return False
     else:
         s = list(''.join(s))
         print(s)
         if 2 <= s <= 6: #This checks the lenght of the word.
-            if s[0].lower() and s[1].lower() in alpha: #Check if the first two 
+            if s[0].lower() and s[1].lower() in alpha: #Check if the first two are letters.
 
 
 
