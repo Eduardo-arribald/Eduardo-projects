0c15f3c4833cb6226325a9d909e1431a88a99a03
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 20:54:16 +0000

    Sat, Jan 7, 2023, 8:54 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -39,7 +39,7 @@ def is_valid(s):
                     if s[i] in numbers and i+1 < len(s) and s[i+1] not in numbers:
                         #print("There are numbers in the middle.")
                         return False
-                    elif s[i] == '0' and s[i-1] in alpha:
+                    elif s[i] == '0' and s[i-1] in alpha or :
                         #print("Zero in the middle.")
                         return False
                     else:
