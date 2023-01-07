043bfeee69e58757e3e05de7ac70a273fa931870
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 19:13:32 +0000

    Sat, Jan 7, 2023, 7:13 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,12 +7,19 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-    s = s.split()
     alpha = (
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
         'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u',
         'v', 'w', 'x', 'y', 'z'
     )
-    
+    #I take off the spaces before and after the text.
+    s = s.split() #A list of the word given.
+    if s[1] is not None:
+        return False
+    else:
+        s = list(s)
+        if s[0:2].lower() not in alpha:
+            print("No")
+            return False
 
 main()
