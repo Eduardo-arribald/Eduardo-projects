e0470b7cc06a8e6fd7c058310a655864490de053
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 06:02:09 +0000

    Sat, Jan 7, 2023, 6:02 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -13,7 +13,7 @@ def main():
 def convert(time):
     n = (''.join(time.split())).split(":")
     h, m = n[0], n[1]
-    new = round((int(h) + (int(m)/60)), 1)
+    new = round((int(h) + (int(m)/60)), 2)
     return new
 
 if __name__ == "__main__":
