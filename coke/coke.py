c169365be282d75db5f071853ffc7948e879e357
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 16:58:51 +0000

    Sat, Jan 7, 2023, 4:58 PM +00:00

---
+++ coke/coke.py
@@ -0,0 +1,14 @@
+
+def main():
+    while True:
+        x = 50
+        print(f"Amount due: {x}")
+        coin = input()
+
+
+def machine(c):
+    coins = ["25", "10", "5"]
+    if c in coins:
+        c = int(c)
+
+
