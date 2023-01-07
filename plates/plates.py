fd8bd8a9b0c34a4c27d3911429302433b006b8ca
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-07 20:48:21 +0000

    Sat, Jan 7, 2023, 8:48 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -36,7 +36,7 @@ def is_valid(s):
                         print("You wrote a punctuation symbol.")
                         return False
                 for i in range(len(s)):
-                    if s[-(i+2)] in numbers and s[-(i+1)] not in numbers:
+                    if s[i] in numbers and i+1 < len(s) and s[i+1] not in numbers:
                         print("There are numbers in the middle.")
                         return False
                     elif s[i] == '0' and s[i-1] in alpha:
