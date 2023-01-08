399f16ce1a0d3ea4be76971d7decda667214e079
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 17:11:27 +0000

    Sun, Jan 8, 2023, 5:11 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,10 +1,19 @@
 
 
 def fuel():
-    #while True:
-    data = list(''.join(input("Fraction: ").split()))
-    n = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/"]
-    if i not in n:
+    while True:
+        data = list(''.join(input("Fraction: ").split()))
+        n = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/"]
+        if data[0] or data[2] not in n:
             continue
+        else:
+            if int(data[0])/int(data[2]) <= .01:
+                print("E")
+            elif int(data[0])/int(data[2]) >= .99:
+                print("F")
+            else:
+                print(str(int(data[0])/int(data[2])))
+            break
 
 
+fuel()
