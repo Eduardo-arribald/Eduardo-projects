80c5b205586b6c4e6517e40ba51e25ee4be85e6b
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 17:07:25 +0000

    Sun, Jan 8, 2023, 5:07 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,10 @@
 
-x = int('p')
 
-if x is ValueError:
-    print("No")
-else:
-    print("Shi")
+def fuel():
+    #while True:
+    data = list(''.join(input("Fraction: ").split()))
+    n = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/"]
+    if i not in n:
+            continue
+
+
