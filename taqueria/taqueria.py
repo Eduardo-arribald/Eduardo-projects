dfd6bc523d5dcb41f1da08e6adeaf5bbb4dcd6cc
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 18:44:48 +0000

    Sun, Jan 8, 2023, 6:44 PM +00:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -3,23 +3,25 @@
 def felipe():
     count = 0
     while True:
-        order = input("Item: ").strip().title()
-        menu = {
-            "Baja Taco": 4.00,
-            "Burrito": 7.50,
-            "Bowl": 8.50,
-            "Nachos": 11.00,
-            "Quesadilla": 8.50,
-            "Super Burrito": 8.50,
-            "Super Quesadilla": 9.50,
-            "Taco": 3.00,
-            "Tortilla Salad": 8.00
-            }
-        if menu.get(order) == None:
-            continue
-        else:
-            count += menu.get(order)
-            print("Total: "+ "$" + format(count, ".2f"))
-
+        try:
+            order = input("Item: ").strip().title()
+            menu = {
+                "Baja Taco": 4.00,
+                "Burrito": 7.50,
+                "Bowl": 8.50,
+                "Nachos": 11.00,
+                "Quesadilla": 8.50,
+                "Super Burrito": 8.50,
+                "Super Quesadilla": 9.50,
+                "Taco": 3.00,
+                "Tortilla Salad": 8.00
+                }
+            if menu.get(order) == None:
+                continue
+            else:
+                count += menu.get(order)
+                print("Total: "+ "$" + format(count, ".2f"))
+        except EOFError:
+            break
 
 felipe()
