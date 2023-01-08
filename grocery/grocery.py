c449f6ba5e7d946ed2b2aa28c7927e2a43411e73
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 21:21:32 +0000

    Sun, Jan 8, 2023, 9:21 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,18 +1,19 @@
 
 def grocery_list():
-    count = {}
+    lista = {}
     while True:
         try:
             order = input("Item: ").strip()
-            if order in count.keys()
+            if order in lista.keys()
                 count[order] += 1
             else:
                 
+
                 print(count)
                 break
 
         except EOFError:
-            print(sep = "\n")
+            #print(sep = "\n")
 
             break #print("\nTotal: "+ "$" + format(count, ".2f"))
 
