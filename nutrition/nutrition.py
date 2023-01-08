df4382b1ff36ba536dc4b107d5335f938af2cdf6
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 16:54:05 +0000

    Sun, Jan 8, 2023, 4:54 PM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -4,9 +4,9 @@ def nutrition():
         "apple":130, "avocado":50, "banana":110, "cantaloupe":50, "grapefruit":60, "grapes":90, "honneydew melon":50, "kiwifruit":90, "lemon":15,
         "lime":20, "nectarine":60, "orange":80, "peach":60, "pear":100, "pineapple":50,"plums":70, "strawberries":50, "sweet cherries": 100, "tangerine":50, "watermelon":80
         }
-    x = ' '.join(input("Item: ").split())
+    x = (' '.join(input("Item: ").split())).lower()
     print(x)
-    if x.lower() in fruits.keys():
+    if x in fruits.keys():
         print("Calories:", str(fruits.get(x)))
 
 nutrition()
