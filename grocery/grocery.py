b3e9b85093fc62df4504669e1675b5ac8933c6e8
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 23:16:51 +0000

    Sun, Jan 8, 2023, 11:16 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -15,6 +15,9 @@ def grocery_list():
         except EOFError:
             keys = list(lista.keys())
             keys.sort()
+            #This function takes keys (i) from the dictionary called "lista", and
+            #the value (dict[i]), deppending on the values(already sorted)
+            #from the list called "keys".
             new_list = {i : lista[i] for i in keys}
             for i in new_list.keys():
                 print(new_list.get(i), i)
