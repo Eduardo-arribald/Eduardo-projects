18ee2a4b7641492d67d04d9889c5ab1af070b779
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 17:14:45 +0000

    Sun, Jan 8, 2023, 5:14 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -4,9 +4,8 @@ def fuel():
     while True:
         data = list(''.join(input("Fraction: ").split()))
         n = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/"]
-        print(data)
-        break
-        """
+        print(data[0])
+        print(data[2])
         if data[0] or data[2] not in n:
             continue
         else:
@@ -15,8 +14,7 @@ def fuel():
             elif int(data[0])/int(data[2]) >= .99:
                 print("F")
             else:
-                print(str(int(data[0])/int(data[2])))
-            break"""
-
+                print(str(int(data[0])/int(data[2]))+"%")
+            break
 
 fuel()
