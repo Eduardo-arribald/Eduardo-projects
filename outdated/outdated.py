0bdf7893a37da90e8cbcae2054eb746a8916098a
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 23:42:14 +0000

    Sun, Jan 8, 2023, 11:42 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,11 +1,9 @@
 
 def change_date():
     meses = [
-        "January", "February", "March",    "April",
-        "May", "June", "July", "August", "September",
-        "October", "November", "December"
+        "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
         ]
-
     date = input("Date: ").split()
     if len(date) > 1:
         for i in range(len(date)):
