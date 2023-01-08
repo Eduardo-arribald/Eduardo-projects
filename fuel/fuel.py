89b34fb13a763640c90f3f412fe4f7d324fcfbaf
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 17:25:32 +0000

    Sun, Jan 8, 2023, 5:25 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,22 +1,19 @@
 
 
 def fuel():
-    #while True:
-    try:
-
-        data = list(''.join(input("Fraction: ").split()))
-        x = int(data[0])
-        y = int(data[2])
-        print(str(x))
-        print(str(y))
-        if x/y <= .01:
-            print("E")
-        elif x/y >= .99:
-            print("F")
-        else:
-            print(str(x/y)+"%")
-        #break
-    except ValueError:
-        print("Not a number")
+    while True:
+        try:
+            data = list(''.join(input("Fraction: ").split()))
+            x = int(data[0])
+            y = int(data[2])
+            if x/y <= .01:
+                print("E")
+            elif x/y >= .99:
+                print("F")
+            else:
+                print(str(x/y)+"%")
+            break
+        except ValueError or ZeroDivisionError:
+            continue
 
 fuel()
