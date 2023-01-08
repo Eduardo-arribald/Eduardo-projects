8abf8bf1863f2a4090457afb360d9777cfed7615
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-08 23:56:35 +0000

    Sun, Jan 8, 2023, 11:56 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -5,16 +5,19 @@ def change_date():
         "August", "September", "October", "November", "December"
         ]
     date = input("Date: ").split()
-    print(date)
+    #print(date)
     date_1 = (''.join(date)).split("/")
+    date_2 = list(''.join(date))
     print(date_1)
+    print(date_2)
     if len(date) > 0:
         for i in range(len(date)):
             for n in range(len(meses)):
                 if date[i] == meses[n]:
                     date[i] == n+1
+
     elif "/" in list(date):
-        date = (''.join(date)).split("/")
+        date = ''.join(date).split("/")
     print(date)
 
 change_date()
