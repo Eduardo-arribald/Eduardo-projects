c3dfb82eec6a6b8847b2514eb3ecc986703e96ba
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 22:47:53 +0000

    Mon, Jan 9, 2023, 10:47 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,29 +6,29 @@ figlet = Figlet()
 
 def main():
     fonts = figlet.getFonts()
-    """if there are 2 arguments"""
+    #check if 2 arguments were given.
     if len(sys.argv) == 3:
-        """Check if the first is a valid one and the second is an existing font"""
+        #Check if those arguments are valids.
         if (sys.argv[1] == '-f' or sys.argv[1] == '--font') and sys.argv[2] in fonts:
             #sys.argv[1]
             #figlet.getFonts()
             figlet.setFont(font = sys.argv[2])
             x = input("Input: ")
             print(figlet.renderText(x))
-        """if the first is not a valid command or the second is not in the list of fonts,
-        print an error message."""
+        #if not valids, print an error message.
         else:
             print("Invalid usage")
             print("Invalid usage")
             sys.exit()
+    #if no arguments given, prompt the input message anyway.
     elif len(sys.argv) == 1:
         x = input("Input: ")
         print(figlet.renderText(x))
-    """elif len(sys.argv) == 2:
-        #print("Invalid usage")
+    #If one argument given, prompt an error message.
+    elif len(sys.argv) == 2:
+        print("Invalid usage")
         print("Invalid usage")
         sys.exit()
-"""
 
 main()
 
