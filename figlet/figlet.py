4858b414c221802e68f4cbe8065b2d8f2d8b2ec9
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 22:12:47 +0000

    Mon, Jan 9, 2023, 10:12 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,14 @@
 
 from pyfiglet import Figlet
+import sys
 
 figlet = Figlet()
 
 def main():
+    if len(sys.argv)
+    #sys.argv[1]
     figlet.getFonts()
     figlet.setFont(font = f)
     x = input("Input: ")
     print(figlet.renderText(x))
+
