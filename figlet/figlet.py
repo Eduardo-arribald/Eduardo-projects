bc8942799132c6ee40a64d1f318a1602c834bc9a
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 22:21:56 +0000

    Mon, Jan 9, 2023, 10:21 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,10 +5,19 @@ import sys
 figlet = Figlet()
 
 def main():
-    if len(sys.argv)
-    #sys.argv[1]
-    figlet.getFonts()
-    figlet.setFont(font = f)
-    x = input("Input: ")
-    print(figlet.renderText(x))
+    fonts = figlet.getFonts()
+    if len(sys.argv) == 3:
+        if (sys.argv[1] == '-f' or sys.argv[1] == '-font') and sys.argv[2] in fonts:
+            #sys.argv[1]
+            #figlet.getFonts()
+            figlet.setFont(font = sys.argv[2])
+            x = input("Input: ")
+            print(figlet.renderText(x))
+    elif len(sys.argv) == 1:
+        x = input("Input: ")
+        print(figlet.renderText(x))
+    else:
+        sys.exit()
+main()
 
+#print(figlet.getFonts())
