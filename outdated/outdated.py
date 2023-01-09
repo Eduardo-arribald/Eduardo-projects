9482b40e22786e77e6f785ecfee6130eb370b8b8
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 00:13:00 +0000

    Mon, Jan 9, 2023, 12:13 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -10,14 +10,11 @@ def change_date():
     print(date)
     print(date_1)
     print(date_2)
-    if len(date) > 1:
-        for i in range(len(date)):
-            if date[i] in meses:
-                date[i] = meses.index(date[i]) + 1
-                #print(date)
+    for i in range(len(date)):
+        if date[i] in meses:
+            date[i] = meses.index(date[i]) + 1
+            #print(date)
 
-    elif "/" in list(''.join(date)):
-        date = ''.join(date).split("/")
     #print(date)
 
 change_date()
