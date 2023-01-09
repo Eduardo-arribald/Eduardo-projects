7604c76f865c3198860e01960c11c9ec05998625
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 00:05:36 +0000

    Mon, Jan 9, 2023, 12:05 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -5,9 +5,10 @@ def change_date():
         "August", "September", "October", "November", "December"
         ]
     date = input("Date: ").split()
-    print(date)
     date_1 = (''.join(date)).split("/")
     date_2 = list(''.join(date))
+    
+    print(date)
     print(date_1)
     print(date_2)
     if len(date) > 1:
