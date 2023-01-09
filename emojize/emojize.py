4dfd3d535ead71d32af48a0ad75128d6c1c53ab6
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 21:28:48 +0000

    Mon, Jan 9, 2023, 9:28 PM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,10 +1,11 @@
 
-import emoji
+from emoji import emojize
 
 def search(x:str):
     #x = input()
-    #print(emoji.emojize(x))
-    print(emoji.emojize(x, languague = "alias")) #variant = "emoji_type"))
+    print(emojize(x))
+    print(type(x))
+    #print(emojize(x, languague = 'alias')) #variant = "emoji_type"))
 
-#search(":thumbs_up:")
+search(":thumbs_up:")
 search(":thumbsup:")
