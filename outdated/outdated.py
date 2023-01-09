82af7627ea2477d2f7a148a0352113f5fa68c3ac
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 01:33:43 +0000

    Mon, Jan 9, 2023, 1:33 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -30,8 +30,8 @@ def change_date():
                 to_int(date)
                 return print(f"{date[2]:02}-{date[1]:02}-{date[0]:02}")
         except:
-            #continue
-            break
+            continue
+            #break
 
 def to_int(lista:list):
     for i in range(len(lista)):
