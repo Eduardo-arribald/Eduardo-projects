6146136528400ac8cc21d512b48681e275d54b26
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 00:24:10 +0000

    Mon, Jan 9, 2023, 12:24 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -10,12 +10,20 @@ def change_date():
     print(date)
     if date[0] in meses:
         date[i] = meses.index(date[i]) + 1
-        print()
+        to_int(date)
+        return print(f"{date[2]:02}/{date[1]:02}/{date[0]:02}")
     elif date[1] or date[2] in meses:
         continue
     elif int(date[1]) > 12:
         continue
     else:
-        new_date =
+        return print(f"{date[2]:02}/{date[1]:02}/{date[0]:02}")
+
+def to_int(lista:list):
+    for i in range(len(lista)):
+        try:
+            lista[i] = int(lista[i])
+        except:
+            pass
 
 change_date()
