67ac9b3bf274ed98d02b801a203f73968646e1f4
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 00:19:21 +0000

    Mon, Jan 9, 2023, 12:19 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -10,8 +10,12 @@ def change_date():
     print(date)
     if date[0] in meses:
         date[i] = meses.index(date[i]) + 1
-
+        print()
     elif date[1] or date[2] in meses:
         continue
+    elif int(date[1]) > 12:
+        continue
+    else:
+        new_date =
 
 change_date()
