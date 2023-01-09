9c6aa8d800b8801f434b0c358d1a719b47c505ef
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-09 00:30:14 +0000

    Mon, Jan 9, 2023, 12:30 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -3,24 +3,24 @@ def change_date():
     while True:
         try:
             meses = [
-                "January", "February", "March", "April", "May", "June", "July",
-                "August", "September", "October", "November", "December"
-                ]
+            "January", "February", "March", "April", "May", "June", "July",
+            "August", "September", "October", "November", "December"
+            ]
             date = input("Date: ").split()
             date_1 = ('/'.join(date)).split("/")
             date = (','.join(date_1)).split(",")
             print(date)
             if date[0] in meses:
-                date[i] = meses.index(date[i]) + 1
+                date[0] = meses.index(date[0]) + 1
                 to_int(date)
-                return print(f"{date[2]:02}/{date[1]:02}/{date[0]:02}")
-            """
+                return print(f"{date[2]:02}-{date[1]:02}-{date[0]:02}")
+                """
             elif date[1] or date[2] in meses:
-                continue
+            continue
             elif int(date[1]) > 12:
-                continue"""
+            continue"""
             else:
-                return print(f"{date[2]:02}/{date[1]:02}/{date[0]:02}")
+                return print(f"{date[2]:02}-{date[1]:02}-{date[0]:02}")
         except:
             continue
 
