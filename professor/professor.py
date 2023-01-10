0a090272e642d56a5cb164d4c67f6bcd03c7bb9d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-10 03:20:18 +0000

    Tue, Jan 10, 2023, 3:20 AM +00:00

--- professor/professor.py
+++ professor/professor.py
@@ -3,14 +3,13 @@ import random
 
 def main():
     z = input("Level: ")
+    get_level(z)
 
 
 def get_level(n):
     n = int(n)
     if n in [1, 2, 3]:
-        x = random.ranint()
-
-
+        x = random.ranint(1, (int("1"*n)*9))
 
 
 
