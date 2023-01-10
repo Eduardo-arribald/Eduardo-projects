ba63ddbb59eefebd6d9b172cde887382140f1001
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-10 01:05:57 +0000

    Tue, Jan 10, 2023, 1:05 AM +00:00

--- game/game.py
+++ game/game.py
@@ -6,7 +6,9 @@ guesses = 0
 while True:
     try:
         top_of_range = input("Level: ")
+        print(top_of_range)
         if top_of_range.isdigit() and int(top_of_range) > 0:
+            top_of_range = int(top_of_range)
             random_number = random.randint(0, top_of_range)
             guesses += 1
             guess = int(input("Guess: "))
