efab4457d685f63a85be54dc0dd4ba9689ad8f5f
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-10 01:01:10 +0000

    Tue, Jan 10, 2023, 1:01 AM +00:00

--- game/game.py
+++ game/game.py
@@ -18,28 +18,8 @@ while True:
                 print("Just right!")
                 break
         else:
-            
+            continue
     except:
         break
 
-    else:
-        print('Por favor intenta escribiendo un número.')
-        continue
 
-    guesses += 1
-    user_guess = input(f"Adivina un número entre 0 y {top_of_range}: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    else:
-        print('Por favor escribe un número la próxima vez.')
-        continue
-    if user_guess == random_number:
-        print("Adivinaste!")
-        break
-    else:
-        if user_guess > random_number:
-            print('Estuviste por encima del número, intentalo de nuevo!')
-        elif user_guess < random_number:
-            print("Estuviste por debajo de número, intentalo de nuevo!")
-
-print("Lo adivinaste en " + str(guesses) + " intentos.")
