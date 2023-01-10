0aa4af47960da9be0eaa40df3c5f3f7ed5926801
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-10 00:06:35 +0000

    Tue, Jan 10, 2023, 12:06 AM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -3,6 +3,8 @@ import inflect
 
 p = inflect.engine()
 
+thing = "car"
 
+idea = " digit"
 
-print("Did you want ", p.a(thing), " or ", p.an(idea))
+print("Did you want ", p.a(thing), "or", p.an(idea))
