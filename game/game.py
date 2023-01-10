315a90c7995663e7c69e30f1eff10f6ad3aa5e3f
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-10 00:52:42 +0000

    Tue, Jan 10, 2023, 12:52 AM +00:00

--- game/game.py
+++ game/game.py
@@ -1,2 +1,36 @@
 
 import random
+
+top_of_range = input("Level: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
+        print('Intenta con un número más grande que 0.')
+        quit()
+else:
+    print('Por favor intenta escribiendo un número.')
+    quit()
+
+random_number = random.randint(0, top_of_range)
+
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input(f"Adivina un número entre 0 y {top_of_range}: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Por favor escribe un número la próxima vez.')
+        continue
+    if user_guess == random_number:
+        print("Adivinaste!")
+        break
+    else:
+        if user_guess > random_number:
+            print('Estuviste por encima del número, intentalo de nuevo!')
+        elif user_guess < random_number:
+            print("Estuviste por debajo de número, intentalo de nuevo!")
+
+print("Lo adivinaste en " + str(guesses) + " intentos.")
