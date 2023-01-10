85729d156d26389b98753adb0a6da51adbd64a8d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-10 01:09:09 +0000

    Tue, Jan 10, 2023, 1:09 AM +00:00

--- game/game.py
+++ game/game.py
@@ -5,6 +5,7 @@ guesses = 0
 top_of_range = input("Level: ")
 
 while True:
+
     try:
         if top_of_range.isdigit() and int(top_of_range) > 0:
             top_of_range = int(top_of_range)
@@ -13,14 +14,17 @@ while True:
             guess = int(input("Guess: "))
             if guess < random_number:
                 print("Too small!")
+                continue
             elif guess > random_number:
                 print("Too large!")
+                continue
             else:
                 print("Just right!")
                 break
         else:
             continue
     except:
-        break
+        continue
 
 
+10
