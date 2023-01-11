b00ee6770acee279297d542d646a9b283fe395ee
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-11 23:44:47 +0000

    Wed, Jan 11, 2023, 11:44 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,15 +5,14 @@ import requests
 try:
     x = input("Request: ")
     x = float(x)
-
-    {"time":{"updated":"Jan 11, 2023 23:40:00 UTC","updatedISO":"2023-01-11T23:40:00+00:00","updateduk":"Jan 11, 2023 at 23:40 GMT"},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","chartName":"Bitcoin","bpi":{"USD":{"code":"USD","symbol":"&#36;","rate":"17,888.0105","description":"United States Dollar","rate_float":17888.0105},"GBP":{"code":"GBP","symbol":"&pound;","rate":"14,947.0785","description":"British Pound Sterling","rate_float":14947.0785},"EUR":{"code":"EUR","symbol":"&euro;","rate":"17,425.5339","description":"Euro","rate_float":17425.5339}}}
-
+    price = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    print(price)
 
 except requests.RequestException:
     print("Problema Request")
 
 
-    {"time":{"updated":"Jan 11, 2023 23:40:00 UTC","updatedISO":"2023-01-11T23:40:00+00:00","updateduk":"Jan 11, 2023 at 23:40 GMT"},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","chartName":"Bitcoin","bpi":{"USD":{"code":"USD","symbol":"&#36;","rate":"17,888.0105","description":"United States Dollar","rate_float":17888.0105},"GBP":{"code":"GBP","symbol":"&pound;","rate":"14,947.0785","description":"British Pound Sterling","rate_float":14947.0785},"EUR":{"code":"EUR","symbol":"&euro;","rate":"17,425.5339","description":"Euro","rate_float":17425.5339}}}
+    #{"time":{"updated":"Jan 11, 2023 23:40:00 UTC","updatedISO":"2023-01-11T23:40:00+00:00","updateduk":"Jan 11, 2023 at 23:40 GMT"},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","chartName":"Bitcoin","bpi":{"USD":{"code":"USD","symbol":"&#36;","rate":"17,888.0105","description":"United States Dollar","rate_float":17888.0105},"GBP":{"code":"GBP","symbol":"&pound;","rate":"14,947.0785","description":"British Pound Sterling","rate_float":14947.0785},"EUR":{"code":"EUR","symbol":"&euro;","rate":"17,425.5339","description":"Euro","rate_float":17425.5339}}}
 
 #Excepts breaks the program if the input isn't a number.
 except ValueError:
