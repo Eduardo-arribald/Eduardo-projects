d8024048aacaf810a6d54782e2cb66b7029593fd
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-14 22:32:19 +0000

    Sat, Jan 14, 2023, 10:32 PM +00:00

---
+++ Class/5-Unit_Tests/notes.py
@@ -0,0 +1,81 @@
+""" Unit Test
+
+Is used when testing code
+
+> assert: a key function used to assert/claim that something is true.
+If it is, nothing happens. But if itsn't true, it pop ups an error.
+Error called "AssertionError".
+
+>
+
+- Modules:
+
+> pytest: automates the testing of my code. Adapts some conventions
+so I don't have to write some code parts by myself.
+documentation: docs.pytest.org
+
+"""
+
+import pytest
+from calculator_1 import square
+
+#To run the tests in a better way and debug it, I can run in the prompt:
+# pytest file_name.py
+#And it will print the mistakes in the code. It only prints one mistake/passed per function.
+
+def main():
+    #test_square()
+    test_positives()
+    test_negatives()
+    test_zero()
+
+def test_square():
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared was not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared was not 9")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared was not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared was not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("3 squared was not 9")
+
+    ns = [-3, -2, 0, 2, 3]
+
+    print("Loop test:")
+
+    for i in ns:
+        try:
+            assert square(i) == i*i
+        except AssertionError:
+            print(f"{i} squared was not {i*i}")
+
+
+def test_positives():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negatives():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
+
+
+if __name__=="__main__":
+    main()
+
