8c005bce22a2570e0497c0d353304759dd28239b
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 21:25:07 +0000

    Mon, Jan 16, 2023, 9:25 PM +00:00

---
+++ test_twttr/test_twttr.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    ...
+
+
+def shorten(word:str):
+    vowels = ("a", "e", "i", "o", "u")
+    word = list(' '.join(word.split()))
+    n = []
+    for l in range(len(w)):
+        if w[l].lower() not in vowels:
+            n.append(w[l])
+    return (''.join(n))
+
+
+"""
+if __name__ == "__main__":
+    main()
+"""
+
