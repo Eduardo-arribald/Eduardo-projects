1c9153d27fef16f45409d5492a42bb3db132a783
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 22:36:36 +0000

    Mon, Jan 16, 2023, 10:36 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -9,11 +9,14 @@ def test_valids():
     assert is_valid("ECTO88") == True
     assert is_valid("NRVOUS") == True
 
+
+
 def test_invalids():
     assert is_valid("CS05") == False
     assert is_valid("CS50P2") == False
     assert is_valid("PI3.14") == False
     assert is_valid("H") == False
+    assert is_valid("3FG2") == False
     assert is_valid("OUTATIME") == False
 
 
