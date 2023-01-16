edf8800f9c14b2b11069816e43abdef5688adeb1
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 22:32:38 +0000

    Mon, Jan 16, 2023, 10:32 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,13 +1,20 @@
 from plates import is_valid
 
 def main():
-    #test_numbers()
+    test_valids()
+    test_invalids()
 
 
-def test_hello():
-    assert value("Hello") == 0
+def test_valids():
+    assert value("ECTO88") == True
+    assert value("NRVOUS") == True
 
-
+def test_invalids():
+    assert value("CS05") == False
+    assert value("CS50P2") == False
+    assert value("PI3.14") == False
+    assert value("H") == False
+    assert value("OUTATIME") == False
 
 
 if __name__ == "__main__":
