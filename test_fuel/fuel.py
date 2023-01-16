b0cdec05922c7f98709557b6be3045eb48327a4a
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 23:32:24 +0000

    Mon, Jan 16, 2023, 11:32 PM +00:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,31 @@
+
+#data = (''.join(input("Fraction: ").split())).split("/")
+
+#print(data)
+
+#x = int(data[0])
+#y = int(data[2])
+#print(x/y)
+
+def fuel():
+    while True:
+        try:
+            data = (''.join(input("Fraction: ").split())).split("/")
+            x = int(data[0])
+            y = int(data[1])
+            #print(x/y)
+            if x/y <= .01:
+                print("E")
+            elif 1 >= x/y >= .99:
+                print("F")
+            elif x/y > 1:
+                continue
+            else:
+                print(str(int(100*round(x/y, 2)))+"%")
+            break
+        except ZeroDivisionError:
+            pass
+        except ValueError:
+            pass
+
+fuel()
---
+++ test_fuel/test_fuel.py
@@ -0,0 +1,24 @@
+from fuel import is_valid
+
+def main():
+    test_valids()
+    test_invalids()
+
+
+def test_valids():
+    assert is_valid("ECTO88") == True
+    assert is_valid("NRVOUS") == True
+
+
+
+def test_invalids():
+    assert is_valid("CS05") == False
+    assert is_valid("CS50P2") == False
+    assert is_valid("PI3.14") == False
+    assert is_valid("H") == False
+    assert is_valid("34ERT") == False
+    assert is_valid("OUTATIME") == False
+
+
+if __name__ == "__main__":
+    main()
