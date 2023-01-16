747429012704752d74fa4741fd05429795c3a4d5
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 22:00:59 +0000

    Mon, Jan 16, 2023, 10:00 PM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -4,7 +4,7 @@ from twttr import shorten
 def main():
     test_names()
     test_phrases()
-    test_numbers()
+    #test_numbers()
 
 def test_names():
     assert shorten("EDUARDO") == "DRD"
@@ -15,9 +15,9 @@ def test_phrases():
     assert shorten("HOLA, MI AMORCITO") == "HL, M MRCT"
     assert shorten("BUENAS NOCHES!") == "BNS NCHS!"
 
-def test_numbers():
-    assert shorten("2 tacos, por favor") == " tcs, pr fvr"
-    assert shorten("Gano 3000 pesos al mes") == "Gn  pss l ms"
+#def test_numbers():
+    #assert shorten("2 tacos, por favor") == " tcs, pr fvr"
+    #assert shorten("Gano 3000 pesos al mes") == "Gn  pss l ms"
 
 
 if __name__=="__main__":
