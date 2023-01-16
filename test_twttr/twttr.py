835003d1d831f5bdb2685d3a47536cd555097fd8
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 21:51:03 +0000

    Mon, Jan 16, 2023, 9:51 PM +00:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -6,11 +6,12 @@ def main():
 
 def shorten(word:str):
     vowels = ("a", "e", "i", "o", "u")
+    numbs = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
     word = list(' '.join(word.split()))
     n = []
     for l in range(len(word)):
-        #if word[l].lower() not in vowels or word[l].isdigit():
-        if word[l].isdigit():
+        if word[l].lower() not in vowels or word[l] not in numbs:
+        #if word[l].isdigit():
             n.append(word[l])
     return (''.join(n))
 
