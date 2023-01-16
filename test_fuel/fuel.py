14db6b00a77d22265e6982ce943a22d4f8b5fb6d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 23:44:35 +0000

    Mon, Jan 16, 2023, 11:44 PM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -9,35 +9,32 @@
 
 
 def main():
-    convert()
-    gauge()
+    n = input("Fraction: ")
+    n = convert(n)
+    print(gauge(n))
 
 
 def convert(fraction):
-    fraction = (''.join(input("Fraction: ").split())).split("/")
+    fraction = (''.join(fraction.split())).split("/")
     data = fraction
     x = int(data[0])
     y = int(data[1])
     if x > y:
         return ValueError
+    #if y == 0:
+        #return ZeroDivisionError
     #print(x/y)
-    if x/y <= .01:
-        print("E")
-    elif 1 >= x/y >= .99:
-        print("F")
-    elif x/y > 1:
-        continue
     else:
-        print(str(int(100*round(x/y, 2)))+"%")
-    break
-except ZeroDivisionError:
-    pass
-except ValueError:
-    pass
+        return(int(100*round(x/y, 2)))
 
 
 def gauge(percentage):
-    ...
+    if percentage <= .01:
+        return ("E")
+    elif 1 >= percentage >= .99:
+        return ("F")
+    else:
+        return f"{percentage}%"
 
 
 if __name__ == "__main__":
