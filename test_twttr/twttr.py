9bbf7723fb275dc8251df63efa19a78a792f977d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-16 21:27:09 +0000

    Mon, Jan 16, 2023, 9:27 PM +00:00

---
+++ test_twttr/twttr.py
@@ -0,0 +1,22 @@
+
+def main():
+    tweet = input()
+    sorten(tweet)
+
+
+def shorten(word:str):
+    vowels = ("a", "e", "i", "o", "u")
+    word = list(' '.join(word.split()))
+    n = []
+    for l in range(len(w)):
+        if w[l].lower() not in vowels:
+            n.append(w[l])
+    return (''.join(n))
+
+
+"""
+if __name__ == "__main__":
+    main()
+"""
+
+
