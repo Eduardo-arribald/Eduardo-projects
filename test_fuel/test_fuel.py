5d72a542ae754fffb11be1ef78137b29675587ec
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-01-17 00:10:52 +0000

    Tue, Jan 17, 2023, 12:10 AM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,18 +1,21 @@
 from fuel import convert, gauge
 
 def main():
-    test_valids()
-    test_invalids()
+    test_convert()
+    test_gauge()
 
 
 def test_convert():
-    assert convert("ECTO88") == True
-    assert convert("NRVOUS") == True
+    assert convert("3/4") == 75
+    assert convert("4/4") == 100
+    assert convert("5/4") == ValueError
+    assert convert("4/0") == ZeroDivisionError
+
 
 def test_gauge():
-    assert gauge("ECTO88") == True
-    assert gauge("NRVOUS") == True
-
+    assert gauge(100) == "F"
+    assert gauge(1) == "E"
+    assert gauge(75) == "75%"
 
 if __name__ == "__main__":
     main()
