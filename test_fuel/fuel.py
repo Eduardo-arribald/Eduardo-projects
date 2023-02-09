707a4897eb10b388251e52cbb4aaacf598971330
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-09 03:19:55 +0000

    Thu, Feb 9, 2023, 3:19 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -13,24 +13,25 @@ def main():
 
 
 def convert(fraction):
-    while True:
-        try:
-            #split fraction to eliminate the spaces before and after the text.
-            fraction = fraction.split()
-            fraction = (''.join(fraction)).split("/")
-            data = fraction
-            x = int(data[0])
-            y = int(data[1])
-            #print(x)
-            #print(y)
-            #print(data)
-            return (int(100*round(x/y, 2)))
-        except ZeroDivisionError:
-            pass
-        except:
-            pass
-        else:
-            break
+    #while True:
+        #try:
+    #split fraction to eliminate the spaces before and after the text.
+    fraction = fraction.split()
+    fraction = (''.join(fraction)).split("/")
+    data = fraction
+    if y
+    x = int(data[0])
+    y = int(data[1])
+    #print(x)
+    #print(y)
+    #print(data)
+    return (int(100*round(x/y, 2)))
+        #except ZeroDivisionError:
+            #pass
+        #except:
+
+        #else:
+            #break
 
 
 def gauge(percentage):
