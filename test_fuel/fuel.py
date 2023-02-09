867588f544a109a47a7935dd15d90fec3f50080c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-09 03:13:08 +0000

    Thu, Feb 9, 2023, 3:13 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,30 +1,28 @@
 
 
 def main():
-    n = input('Fraction: ')
-    n = convert(n)
-    #while True:
-    n = gauge(n)
-    if type(n) == int and int(n) > 100:
-            #continue
-        #else:
-    print(n)
-        #break
+    while True:
+        n = input('Fraction: ')
+        n = convert(n)
+        n = gauge(n)
+        if type(n) == int and int(n) > 100:
+            continue
+        else:
+            print(n)
+            break
 
 
 def convert(fraction):
     while True:
         try:
-            #print(fraction.split())
             #split fraction to eliminate the spaces before and after the text.
             fraction = fraction.split()
-            #print(fraction)
             fraction = (''.join(fraction)).split("/")
             data = fraction
             x = int(data[0])
             y = int(data[1])
-            print(x)
-            print(y)
+            #print(x)
+            #print(y)
             #print(data)
             return (int(100*round(x/y, 2)))
         except ZeroDivisionError:
@@ -36,7 +34,7 @@ def convert(fraction):
 
 
 def gauge(percentage):
-    print(type(percentage))
+    #print(type(percentage))
     #return percentage
     if float(percentage) <= 1:
         return "E"
@@ -45,7 +43,7 @@ def gauge(percentage):
     elif 1 < float(percentage) < 99:
         return f"{percentage}%"
     else:
-        print("Es mayor a 100")
+        #print("Es mayor a 100")
         return percentage
 
 
