1dffce24c9051338959abe7b2bf52a36033d348f
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-09 19:35:42 +0000

    Thu, Feb 9, 2023, 1:35 PM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -4,18 +4,19 @@ import csv
 count = 0
 with open("example.py") as file:
     for line in file:
-        x = line.split()
-        if len(x) > 0: #and x[0] != '#':
-            y = list(''.join(x))
+        x = line.lstrip()
+        print(x)
+        if x != '' and not x.startswith("print") and not x.startswith("#"):
+            #y = list(''.join(x))
             #print(y)
-            if y[0] != '#':
+            #if file != '#':
                 #print(line)
-                count +=1
+            count +=1
 
 word = '     Eduardo'
 
-print(word.lstrip()) #Removes spaces from the left. 
+#print(word.lstrip()) #Removes spaces from the left.
 
-print(word.startswith('E'))
+#print(word.startswith('E'))
 
-#print(str(count))
+print(str(count))
