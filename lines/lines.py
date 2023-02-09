b447d29b7c4b0674f54341f1dcc4def06f67a2df
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-09 19:19:04 +0000

    Thu, Feb 9, 2023, 1:19 PM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -5,7 +5,8 @@ count = 0
 with open("fuel.py") as file:
     for line in file:
         x = line.split()
-        if x[0] is not None:
-            count +=1
+        print(str(len(x)))
+        #if len(x) >0 is not None:
+        count +=1
 
 print(str(count))
