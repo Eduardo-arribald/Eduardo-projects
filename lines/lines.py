be2b57111a577ced68726d555d9689623e024a0b
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-09 20:23:08 +0000

    Thu, Feb 9, 2023, 2:23 PM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,12 +1,11 @@
-import sys
 
+import sys
 
 def main():
     #Argument 0 is this file name itself.
     #Argument 1 is the file that I need to count for.
     if len(sys.argv) == 2 and sys.argv[1].endswith(".py"):
         x = sys.argv[1]
-        #print(x)
         search_for(x)
     elif len(sys.argv) == 2 and not sys.argv[1].endswith(".py"):
         print("Not a python file")
@@ -22,13 +21,7 @@ def search_for(file):
         with open(file) as file:
             for line in file:
                 x = line.lstrip()
-                #print(x)
                 if x != '' and not x.startswith("#"):
-                    #print(x)
-                    #y = list(''.join(x))
-                    #print(y)
-                    #if file != '#':
-                    #print(line)
                     count +=1
             print(str(count))
     except FileNotFoundError:
