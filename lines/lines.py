4f6e8f48098a9b0dea4841a28c475859f7ec0139
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-09 18:58:04 +0000

    Thu, Feb 9, 2023, 12:58 PM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,6 +1,11 @@
 import sys
 import csv
 
+count = 0
 with open("fuel.py") as file:
     for line in file:
-        print(line)
+        x = line.split()
+        if x[0] is not None:
+            count +=1
+
+print(str(count))
