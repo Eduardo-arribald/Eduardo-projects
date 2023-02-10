a80528fd7a146fa074f0e5da835f6ecda53a3cf1
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 02:14:07 +0000

    Thu, Feb 9, 2023, 8:14 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -10,26 +10,24 @@ def main():
 
 def process_file(x):
     if len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
-        new = x[2][:-4]
-        new = new + ".txt"
-        #print(new)
-        with open(new, "w") as new_file:
+        with open(x[2], "w") as new_file:
             headers = ["first", "last", "house"]
-            #writer = csv.DictWriter(new_file, fieldnames = headers)
-            new_file.write(f"{headers[0]},{headers[1]},{headers[2]} \n")
-            #try:
-            with open(x[1]) as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    last, first = row["name"].split(',')
-                    first = first.lstrip()
-                    house = row["house"]
-                    #writer.writeheader()
-                    #writer.writerow({
-                    new_file.write(f"{first},{last},{house} \n")
-                        #})
-            #except:
-                #sys.exit(f"Could not read {x[1]}")
+            writer = csv.DictWriter(new_file, fieldnames = headers)
+            try:
+                with open(x[1]) as file:
+                    reader = csv.DictReader(file)
+                    for row in reader:
+                        last, first = row["name"].split(',')
+                        first = first.lstrip()
+                        house = row["house"]
+                        writer.writeheader()
+                        writer.writerow({
+                            "first":first,
+                            "last":last,
+                            "house": house
+                            })
+            except:
+                sys.exit(f"Could not read {x[1]}")
     elif len(x) > 3:
         sys.exit("Too many command-line arguments")
     elif len(x) < 3:
