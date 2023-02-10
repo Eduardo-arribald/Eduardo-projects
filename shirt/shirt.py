f969fb0de7521ee74c768840d1628eb37b3c648f
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 02:57:32 +0000

    Thu, Feb 9, 2023, 8:57 PM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -18,14 +18,20 @@ def costumes(file):
         images = []
         x = splitext(file)
         print(x)
-        p_1 = Image.open(x[0])
-        p_2 = ImageOps.fit(p_1)
+        #p_1 = Image.open(x[0])
+        #p_2 = ImageOps.fit(p_1)
+        #Image.paste
+        #Image.save
 
         #for arg in sys.argv:
      #   image = Image.open(arg)
       #  images.append(image)
     elif len(file) > 3:
-
+        sys.exit("Too many command-line arguments")
+    elif len(file) < 3:
+        sys.exit("Too few command-line arguments")
+    elif x[1] not in extensions:
+        sys.exit("")
 
     """
     image1 = Image.open("costume1.gif")
