c7e1a07114d1a7a0c48949ac84e4f978bc09b882
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 02:41:11 +0000

    Thu, Feb 9, 2023, 8:41 PM -06:00

---
+++ shirt/shirt.py
@@ -0,0 +1,30 @@
+
+import sys
+from PIL import Image
+
+def main():
+    files = sys.argv
+    files_1 = [1, "before.jpg", "after.jpg"]
+    files_2 = [1, "before.jpg", "after.jpg"]
+    files_3 = [1, "before.jpg", "after.jpg"]
+    costumes(file_1)
+
+
+def costumes():
+    images = []
+    #for arg in sys.argv:
+     #   image = Image.open(arg)
+      #  images.append(image)
+
+    image1 = Image.open("costume1.gif")
+    image2 = Image.open("costume2.gif")
+    images.append(image1)
+    images.append(image2)
+
+    images[0].save(
+        "costumes.gif",
+        save_all = True,
+        append_images = [images[1]],
+        duration = 200,
+        loop = 0
+    )
