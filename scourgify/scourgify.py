cdbe62ca2f38dd9bcd6e0f4567aeea7269f7fefb
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 01:17:29 +0000

    Thu, Feb 9, 2023, 7:17 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,19 +4,19 @@ import sys
 
 def main():
     #file = sys.argv()
-    file = [1, "before.csv", "after.csv"]
-    read_file(file)
+    f = [1, "before.csv", "after.csv"]
+    read_file(f)
 
 
 def read_file(x):
-    if len(x) == 3 and x[1].endswith(".csv")
-    with open(x) as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                students.append({
-                    "name": row['name'],
-                    "home": row['home'],
-                    })
+    if len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
+        with open(x[1]) as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    students.append({
+                        "name": row['name'],
+                        "home": row['home'],
+                        })
 
     name = input("What's your name? ")
     home = input("What's your home? ")
