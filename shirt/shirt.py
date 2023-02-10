2ea31244abc2555f70e742a58802c5bbb75fa0e6
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 03:54:42 +0000

    Thu, Feb 9, 2023, 9:54 PM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -32,7 +32,7 @@ def costumes(file):
                 print("Mupet:",mupet.size)
                 mupet_size = mupet.size
 
-                shirt = op.fit(shirt, size = mupet.size)
+                shirt = op.fit(shirt, size = mupet.size, centering = (0.5, 0.5))
                 print("Shirt:",shirt.size)
                 mupet.paste(shirt, shirt)
                 mupet.save("new_shirt.png")
