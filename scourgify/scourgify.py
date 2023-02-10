b12ae2c08867f169be7b315dea61f5835a4a636e
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 01:57:23 +0000

    Thu, Feb 9, 2023, 7:57 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,41 +3,35 @@ import csv
 import sys
 
 def main():
-    #file = sys.argv()
-    file = [1, "before.csv", "hola.csv", "Si"]
-    print(len(file))
+    file = sys.argv()
+    #file = [1, "before.csv", "after.csv"]
     process_file(file)
 
 
 def process_file(x):
-    try:
-        if len(x) > 3:
-            sys.exit("Too many command-line arguments")
-        elif len(x) < 3:
-            sys.exit("Too few command-line arguments")
-
-        elif len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
-            with open(x[2], "w") as new_file:
-                headers = ["first", "last", "house"]
-                writer = csv.DictWriter(new_file, fieldnames = headers)
-
+    if len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
+        with open(x[2], "w") as new_file:
+            headers = ["first", "last", "house"]
+            writer = csv.DictWriter(new_file, fieldnames = headers)
+            try:
                 with open(x[1]) as file:
                     reader = csv.DictReader(file)
                     for row in reader:
                         last, first = row["name"].split(',')
                         first = first.lstrip()
                         house = row["house"]
-                        #print(row["name"], "=", first)
-                        #print(row)
                         writer.writeheader()
                         writer.writerow({
                             "first":first,
                             "last":last,
                             "house": house
                             })
-
-    except:
-        sys.exit(f"Could not read {x[1]}")
+            except:
+                sys.exit(f"Could not read {x[1]}")
+    elif len(x) > 3:
+        sys.exit("Too many command-line arguments")
+    elif len(x) < 3:
+        sys.exit("Too few command-line arguments")
 
 
 main()
