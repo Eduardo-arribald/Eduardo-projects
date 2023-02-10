81cfc33681e844edf93a7c19916726ef9869e4c1
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 01:19:26 +0000

    Thu, Feb 9, 2023, 7:19 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -11,15 +11,16 @@ def main():
 def read_file(x):
     if len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
         with open(x[1]) as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    students.append({
-                        "name": row['name'],
-                        "home": row['home'],
-                        })
+            reader = csv.DictReader(file)
+            for row in reader:
+                print(row)
+#                    .append({
+#                        "name": row['name'],
+#                        "home": row['home'],
+#                        })
 
-    name = input("What's your name? ")
-    home = input("What's your home? ")
-    with open("students.csv", "w") as file:
-        writer = csv.DictWriter(file, fieldnames = ["name", "home"])
-        writer.writerow({"name":name, "home":home})
+#    name = input("What's your name? ")
+#    home = input("What's your home? ")
+#    with open("students.csv", "w") as file:
+#        writer = csv.DictWriter(file, fieldnames = ["name", "home"])
+#        writer.writerow({"name":name, "home":home})
