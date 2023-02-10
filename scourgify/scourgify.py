c7cb3611550dc25adc6b15aa7f31b72a7060ab64
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 01:33:55 +0000

    Thu, Feb 9, 2023, 7:33 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -13,16 +13,19 @@ def read_file(x):
     if len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
 
         with open(x[2], "w") as new_file:
-
-            with open(x[1]) as file and with open(x[2]) as new_file:
+            headers = ["first", "last", "house"]
+            writer = csv.DictWriter(new_file, fieldnames = headers)
+            with open(x[1]) as file:
                 reader = csv.DictReader(file)
                 for row in reader:
-
+                    name = row["name"]
+                    house = row["house"]
+                    print(name)
                     #print(row)
-                    wili.append({
-                        "name": row['name'],
-                        "home": row['house'],
+                    writer.writerow({
+                        "first":name
                         })
+                    break
 
 #    name = input("What's your name? ")
 #    home = input("What's your home? ")
