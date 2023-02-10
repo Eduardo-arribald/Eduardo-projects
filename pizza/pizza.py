182576977a36427abb87a6138f96742b69d5d482
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 00:24:20 +0000

    Thu, Feb 9, 2023, 6:24 PM -06:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -3,21 +3,27 @@ import sys
 import tabulate
 
 def main():
-    file = sys.argv
+    #file = sys.argv
+    file = ["", "regular.csv"]
     check_for(file)
 
 def check_for(x):
-    try:
-        if len(x) == 2 and x[1].endswith(".csv"):
-            x = x[1]
-            with open(file) as file:
-                for line in file:
+    #try:
+    print(x)
+    if len(x) == 2 and x[1].endswith(".csv"):
+        x = x[1]
 
-        elif len(x) < 2:
-            sys.exit("Too feo commnand-line arguments")
-        elif len(x) == 2 and not x[1].endswith(".csv"):
-            sys.exit("Not a CSV file")
-        elif len(x) > 2:
-            sys.exit("Too many arguments")
-    except FileNotFoundError:
-        sys.exit("File does not exist")
+        table = []
+        with open(file) as file:
+            for line in file:
+                table.append(line)
+
+        print(table)
+    elif len(x) < 2:
+        sys.exit("Too feo commnand-line arguments")
+    elif len(x) == 2 and not x[1].endswith(".csv"):
+        sys.exit("Not a CSV file")
+    elif len(x) > 2:
+        sys.exit("Too many arguments")
+    #except FileNotFoundError:
+        #sys.exit("File does not exist")
