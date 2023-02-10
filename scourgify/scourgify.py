67bae59d483b1c1c3a557b91e7a8da0d06bc9ddb
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 01:53:46 +0000

    Thu, Feb 9, 2023, 7:53 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,16 +4,22 @@ import sys
 
 def main():
     #file = sys.argv()
-    file = [1, "befor.csv", "hola.csv", "Si"]
+    file = [1, "before.csv", "hola.csv", "Si"]
+    print(len(file))
     process_file(file)
 
 
 def process_file(x):
     try:
-        if len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
+        if len(x) > 3:
+            sys.exit("Too many command-line arguments")
+        elif len(x) < 3:
+            sys.exit("Too few command-line arguments")
+
+        elif len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
             with open(x[2], "w") as new_file:
                 headers = ["first", "last", "house"]
-                writer = csv.DictWriter(new_file, fieldnames = ["first", "last", "house"])
+                writer = csv.DictWriter(new_file, fieldnames = headers)
 
                 with open(x[1]) as file:
                     reader = csv.DictReader(file)
@@ -29,10 +35,7 @@ def process_file(x):
                             "last":last,
                             "house": house
                             })
-        elif len(x) > 3:
-            sys.exit("Too many command-line arguments")
-        elif len(x) < 3:
-            sys.exit("Too few command-line arguments")
+
     except:
         sys.exit(f"Could not read {x[1]}")
 
