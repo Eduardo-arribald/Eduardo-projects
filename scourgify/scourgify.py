8c42c573efdbf46223584b44bfbdccb839044a37
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 02:09:12 +0000

    Thu, Feb 9, 2023, 8:09 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -10,9 +10,13 @@ def main():
 
 def process_file(x):
     if len(x) == 3 and x[1].endswith(".csv") and x[2].endswith(".csv"):
-        with open(x[2], "w") as new_file:
+        new = x[2][:-4]
+        new = new + ".txt"
+        print(new)
+        with open(new, "w") as new_file:
             headers = ["first", "last", "house"]
             #writer = csv.DictWriter(new_file, fieldnames = headers)
+            new_file.write(f"{headers[0]},{headers[1]},{headers[2]}")
             try:
                 with open(x[1]) as file:
                     reader = csv.DictReader(file)
@@ -20,12 +24,10 @@ def process_file(x):
                         last, first = row["name"].split(',')
                         first = first.lstrip()
                         house = row["house"]
-                        writer.writeheader()
-                        writer.writerow({
-                            "first":first,
-                            "last":last,
-                            "house": house
-                            })
+                        #writer.writeheader()
+                        #writer.writerow({
+                        new_file.write([f"{first},{last},{house}"])
+                            #})
             except:
                 sys.exit(f"Could not read {x[1]}")
     elif len(x) > 3:
