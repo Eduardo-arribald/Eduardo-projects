013211fb674fa502d2ce805991ba9f6a39cacc2d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-10 01:07:28 +0000

    Thu, Feb 9, 2023, 7:07 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,3 +1,16 @@
 
 import csv
 
+with open("students.csv") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            students.append({
+                "name": row['name'],
+                "home": row['home'],
+                })
+
+        name = input("What's your name? ")
+    home = input("What's your home? ")
+    with open("students.csv", "a") as file:
+        writer = csv.DictWriter(file, fieldnames = ["name", "home"])
+        writer.writerow({"name":name, "home":home})
