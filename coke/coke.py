d330677f6976f70871f8e91d960dbe23ff1eec01
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-11 19:17:16 +0000

    Sat, Feb 11, 2023, 1:17 PM -06:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,11 +4,16 @@
 def main():
     money = 0
     while True:
-        x = 50
+        x = 50 #The cost of a soda
+        #Amount due:
         print(f"Amount due: {x-money}")
+        #The input to put the coin.
         coin = input("Insert Coin: ")
+        #This adds the coin introduced to the money that's already in the machine.
         money = money + check_coins(coin)
+        #This checs if the amount payed is equal or above 50.
         if money >= 50:
+            #Prints the change based on the money introduced. 
             print(f"Change Owed: {money - x}")
             break
 
