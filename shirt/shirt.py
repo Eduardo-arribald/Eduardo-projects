a0b8a9181e6671b29ec18419655115e783148a7b
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-15 17:32:52 +0000

    Wed, Feb 15, 2023, 11:32 AM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -41,13 +41,13 @@ def costumes(file):
                 w = mupet.size[0]
                 l = mupet.size[1]
                 mupet_size = (w, l -150)
-                shirt = op.fit(shirt, size = mupet_size,centering = (.9, 0))
+                shirt = op.fit(shirt, size = mupet_size,centering = (.6, .6))
                 #shirt_2 = Image.resize(mupet_size)
                 #print("Shirt:",shirt.size)
                 mupet.paste(shirt, shirt)
                 mupet = op.fit(mupet, size =mupet_size)
                 mupet.save(new_mupet)
-                m
+                mupet.show()
                 #p_2 = ImageOps.fit(p_1)
                 #Image.paste
                 #Image.save
