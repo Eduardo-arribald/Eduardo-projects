fa9f34d659df02b2476d9421d0a9e713b922ee25
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-15 16:40:39 +0000

    Wed, Feb 15, 2023, 10:40 AM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -9,13 +9,13 @@ def main():
     files_1 = [1, "before1.jpg", "after.jpg"]
     files_2 = [1, "before1.png", "after.jpg"] #check. Exits. Input and output have different extensions
     files_3 = [1, "before1.png", "after.png"] #check. Exits. Input and output have different extensions
-    files_4 = [1, "before1.png", "after.png"]
-    files_5 = [1, "before1.png", "after.png", "long.png"] #check. Exits. Too many command-line arguments
-    files_6 = [1, "before1.png"] #check. Exits. Too few command-line arguments
+    files_4 = [1, "before1.jpg", "after.jpg"]
+    files_5 = [1, "before1.jpg", "after.jpg", "long.jpg"] #check. Exits. Too many command-line arguments
+    files_6 = [1, "before1.jpg"] #check. Exits. Too few command-line arguments
     files_7 = [1] #check. Exits
     files_8 = [1, "before1.gif", "after.gif"] #check. Exits. Invalid input
-    files_8 = [1, "before1.png", "after.gif"] #Exits. Invalid output
-    costumes(files_3)
+    files_8 = [1, "before1.jpg", "after.gif"] #Exits. Invalid output
+    costumes(files_4)
 
 
 def costumes(file):
@@ -31,25 +31,25 @@ def costumes(file):
             mupet = file[1]
             new_mupet = file[2]
             print(f'{mupet} and {new_mupet}')
-            #try:
-            with Image.open(mupet) as mupet:
-                shirt = Image.open("shirt.png")
-                #print("Shirt:",shirt.size)
-                #print("Mupet:",mupet.size)
-                #mupet_size = mupet.size
-                w = mupet.size[0]
-                l = mupet.size[1]
-                mupet_size = (w , l -150)
-                shirt = op.fit(shirt, size = mupet_size)
-                #print("Shirt:",shirt.size)
-                mupet.paste(shirt, shirt)
-                #mupet = op.fit(mupet)
-                mupet.save("new_shirt.png")
-                #p_2 = ImageOps.fit(p_1)
-                #Image.paste
-                #Image.save
-            #except:
-                #sys.exit("Invalid input")
+            try:
+                with Image.open(mupet) as mupet:
+                    shirt = Image.open("shirt.png")
+                    #print("Shirt:",shirt.size)
+                    #print("Mupet:",mupet.size)
+                    #mupet_size = mupet.size
+                    w = mupet.size[0]
+                    l = mupet.size[1]
+                    mupet_size = (w , l -150)
+                    shirt = op.fit(shirt, size = mupet_size)
+                    #print("Shirt:",shirt.size)
+                    mupet.paste(shirt, shirt)
+                    #mupet = op.fit(mupet)
+                    mupet.save("new_shirt.png")
+                    #p_2 = ImageOps.fit(p_1)
+                    #Image.paste
+                    #Image.save
+            except:
+                sys.exit("Invalid input")
         else:
             sys.exit("Input and output have different extensions")
 
