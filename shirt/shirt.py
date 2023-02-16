0dc0416d6c432c52dbc8e9ddebde932cac1d4164
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-16 02:18:06 +0000

    Wed, Feb 15, 2023, 8:18 PM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -40,7 +40,9 @@ def costumes(file):
                     #scales = (scales[0]*w_s, scales[1]*l_s)
                     #print(scales)
                     shirt_scaleted = op.scale(image = shirt, factor = scales[0])
+
                     mupet_size = (w, l)
+                    shirt = op.fit(shirt, size = (w, l-400))
                     #mupet.paste(shirt_scaleted, box = (0, 200), mask = shirt_scaleted)
                     mupet.paste(shirt_scaleted, mask = shirt_scaleted)
                     #mupet = op.fit(mupet, size = (w, l-400))
