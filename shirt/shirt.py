7b04ca678aa64c24e21a9ccb75e5d50185998025
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-02-16 16:16:06 +0000

    Thu, Feb 16, 2023, 10:16 AM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -31,8 +31,8 @@ def costumes(file):
             #Here is where I have to create the wished picture.
             with Image.open(mupet) as mupet:
                 with Image.open("shirt.png") as shirt:
-                    #accurate(shirt, mupet, new_mupet)
-                    homework_way(shirt, mupet, new_mupet)
+                    accurate(shirt, mupet, new_mupet)
+                    #homework_way(shirt, mupet, new_mupet)
 
             #except:
                 #sys.exit("Invalid input")
