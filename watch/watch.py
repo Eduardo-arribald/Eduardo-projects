0000e6b016beae80ec2657970e7e22c9e3ab5b4c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-20 01:40:59 +0000

    Sun, Mar 19, 2023, 7:40 PM -06:00

--- watch/watch.py
+++ watch/watch.py
@@ -3,25 +3,26 @@ import sys
 
 
 def main():
-    x = '<iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>'
+    #x = '<iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>'
     #x_1 ='<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
     #x_2 = '<iframe width="560" height="315" src="https://cs50.harvard.edu/python"></iframe>'
     #x_3 = "https://youtube.com/embed/xvFZjo5PgG0"
-    parse(x)
-    #print(parse(input("HTTML: ")))
+    #parse(x_1)
+    print(parse(input("HTTML: ")))
 
 def parse(s):
     w = re.search(r'(?:http|https)://(?:www.)?youtube.com/embed/(\w+)', s)
     #w = re.search(r"(embed/([a-zA-Z0-9]+))", s)
     #w = re.search(r'"', s)
     if w:
-        print("Simon")
-        print(w.group(1))
+        #print("Simon")
+        #print(w.group(1))
         x = w.group(1)
         z = "https://youtu.be/"
         #x = (w.group(1)).split('"')
-        x = ''.join(z, x)
-        print(x)
+
+        x = z+x
+        return x
     else:
         print("Nel")
 
