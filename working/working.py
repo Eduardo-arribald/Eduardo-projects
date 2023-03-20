fb625d0c069c2455f46df6358735642a28f14510
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-20 02:22:00 +0000

    Sun, Mar 19, 2023, 8:22 PM -06:00

--- working/working.py
+++ working/working.py
@@ -5,15 +5,19 @@ import sys
 
 def main():
     x = "9:00 AM to 5:00 PM"
-    print(convert(x))
+    #print(convert(x))
+    convert(x)
     #print(convert(input("Hours: ")))
 
 
 def convert(s):
     #w = re.search(r'"(?:http|https)://(?:www.)?youtube.com/embed/(\w+)"', s)
-    w = re.search(r'(1?[0-9]:[0-6][0-9]) ([AM|PM]) \w (1?[0-9]:[0-6][0-9]) ([AM|PM])', s)
+    w = re.search(r'(1?[0-9](:[0-6][0-9])?) (AM|PM) [a-z]* (1?[0-9](:[0-6][0-9])?) (AM|PM)', s)
     if w:
-
+        print("Simon")
+        print(w.group(1))
+    else:
+        print("Nel")
 
 
 if __name__ == "__main__":
