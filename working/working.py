81d78e1f513c431f5c061b5c1ebf718fd186f13c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-20 02:44:36 +0000

    Sun, Mar 19, 2023, 8:44 PM -06:00

--- working/working.py
+++ working/working.py
@@ -6,17 +6,20 @@ import sys
 def main():
     x = "9:00 AM to 5:00 PM"
     x_1 = "9 AM to 5 PM"
+    x_2 = "5:00 PM to 9:00 AM"
     #print(convert(x))
-    convert(x_1)
+    convert(x_2)
     #print(convert(input("Hours: ")))
 
 
 def convert(s):
     #w = re.search(r'"(?:http|https)://(?:www.)?youtube.com/embed/(\w+)"', s)
-    w = re.search(r'(1[1-2]|[0-9]):?([0-6][0-9])? (AM|PM) ?[a-z]* ?(1?[0-9])(:[0-6][0-9])? (AM|PM)', s)
+    w = re.search(r'(1[1-2]|[0-9]):?([0-5][0-9]|60)? (AM|PM) ?[a-z]* ?(1[1-2]|[0-9]):?([0-5][0-9]|60)? (AM|PM)', s)
     if w:
         print("Simon")
         print(w.group(1))
+        print(w.group(2))
+        print(w.group(3))
     else:
         print("Nel")
 
