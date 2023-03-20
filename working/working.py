924977182cd96e78d23476c3193d7819c8679e03
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-20 03:06:47 +0000

    Sun, Mar 19, 2023, 9:06 PM -06:00

--- working/working.py
+++ working/working.py
@@ -8,22 +8,23 @@ def main():
     x_1 = "9 AM to 5 PM"
     x_2 = "5:00 PM to 9:00 AM"
     x_3 = "9 AM to 5:30 PM"
+    x_4 = "12:60 AM to 13:00 PM"
     convert(x_3)
     #print(convert(input("Hours: ")))
 
 def convert(s):
     #w = re.search(r'"(?:http|https)://(?:www.)?youtube.com/embed/(\w+)"', s)
-    w = re.search(r'(1[1-2]|[0-9]):?([0-5][0-9]|60)? (AM|PM) ?[a-z]* ?(1[1-2]|[0-9]):?([0-5][0-9]|60)? (AM|PM)', s)
+    w = re.search(r'(1[1-2]|[0-9]):?([0-5][0-9])? (AM|PM) ?[a-z]* ?(1[1-2]|[0-9]):?([0-5][0-9])? (AM|PM)', s)
     if w:
         print("Simon")
         print(w.group(1)) #hour
         print(w.group(2)) #minutes
         print(w.group(3)) #AM / PM
         hour_1 = int(w.group(1)) #hour
-        minute_1 = int(w.group(2)) #minutes (can be None)
+        #minute_1 = int(w.group(2)) #minutes (can be None)
         time_1 = w.group(3) #AM/PM
         hour_2 = int(w.group(4))
-        minute_2 = int(w.group(5))
+        #minute_2 = int(w.group(5))
         time_2 = w.group(6)
 
     else:
