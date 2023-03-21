a569327466cd169c47c1716eba0db1cd73ae6f1d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-21 15:55:26 +0000

    Tue, Mar 21, 2023, 9:55 AM -06:00

--- um/um.py
+++ um/um.py
@@ -7,15 +7,15 @@ def main():
     x = "hola um asdasd"
     x_1 = "um?"
     x_2 = "yummy"
-    x_3 = "um"
+    x_3 = "Um"
     print(count(x_3))
 
 def count(s):
     #s = s.lower()
-    patron = [r"um[.,?;]+"]
+    patron = [r"um\W+"]
     #w = len(re.findall(r"(um|Um)\W+", s, re.IGNORECASE))
     for p in patron:
-        match = re.findall(p, s, re.IGNORECASE)
+        match = re.findall(p, s, flags = re.IGNORECASE)
 
     #y = re.search()
     return len(match)
