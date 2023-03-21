a6fbd59969d1ef8f70623f2a4357981556a9bc8d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-21 12:41:37 +0000

    Tue, Mar 21, 2023, 6:41 AM -06:00

--- working/test_working.py
+++ working/test_working.py
@@ -6,7 +6,7 @@ def main():
 
 
 def true_test():
-    assert convert("9:00 AM to 5:00 PM") == "9:00 AM to 17:00 PM"
+    assert convert("9:00 AM to 5:00 PM") == "9:00 to 17:00"
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("10 PM to 8 AM") == "22:00 to 08:00"
