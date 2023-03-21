ad804d4305ce2650a19309b0b555bae34e0e852c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-21 14:46:08 +0000

    Tue, Mar 21, 2023, 8:46 AM -06:00

--- um/um.py
+++ um/um.py
@@ -5,10 +5,11 @@ import sys
 def main():
     #print(count(input("Text: ")))
     x = "hola um asdasd"
+    x:1
     print(count(x))
 
 def count(s):
-    return len(re.findall(r"^um$", s))
+    return len(re.findall(r"\W[,.]*um[,.]*\W", s))
 
 
 if __name__ == "__main__":
