b9ba9901cce43a78b190b506c537614643653243
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-21 19:36:49 +0000

    Tue, Mar 21, 2023, 1:36 PM -06:00

--- response/response.py
+++ response/response.py
@@ -1,5 +1,21 @@
 from validator_collection import checkers
 
 x = "arriaga_@outlook.com"
+x_1 = "malan@@@harvard.edu"
 
-print(checkers.is_email(x))
+def main():
+    #emails = ["arriaga_@outlook.com", "malan@harvard.edu", "malan@@@harvard.edu", "arriaga_@outlook..com"]
+    #checker(emails)
+    print(checker(input("What's your email address? ")))
+
+
+def checker(email):
+
+    if checkers.is_email(email):
+        return "Valid"
+    else:
+        return "Invalid"
+
+
+if __name__ == "__main__":
+    main()
