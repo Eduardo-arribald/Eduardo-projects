23ed303e2ede3f16582555258f361eab49efe71c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-21 12:40:11 +0000

    Tue, Mar 21, 2023, 6:40 AM -06:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,14 +1,23 @@
 from working import convert
 
 def main():
-
+    true_test()
+    false_test()
 
 
 def true_test():
     assert convert("9:00 AM to 5:00 PM") == "9:00 AM to 17:00 PM"
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
-    assert convert("9:00 AM to 5:00 PM") == ""
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
+    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
+
+def false_test():
+    assert convert("9:60 AM to 5:60 PM") == ValueError
+    assert convert("9 AM - 5 PM") == ValueError
+    assert convert("09:00 AM - 17:00 PM") == ValueError
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+
+main()
