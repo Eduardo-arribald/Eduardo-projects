54ea0e2ea8edfc751b5272102cf2e1a9bf5c1f1c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-21 16:18:22 +0000

    Tue, Mar 21, 2023, 10:18 AM -06:00

--- um/um.py
+++ um/um.py
@@ -9,14 +9,15 @@ def main():
     x_2 = "yummy" # 0
     x_3 = "Um" # 1
     x_4 = "hola um, asdasd" # 1
-    print(count(x))
+    x_5 = "Um, thanks, um..." #2
+    print(count(x_5))
 
 def count(s):
     #s = str(s.lower())
     print(s)
     #print(len(list(s)))
-    if " " in s:
-        match = re.findall(r"\W+um\W+", s, flags = re.IGNORECASE)
+    if " " in s:          #W\+
+        match = re.findall(r"[^a-z]um\W+", s, flags = re.IGNORECASE)
         return len(match)
     else:
         match = re.findall(r"um\W*", s, flags = re.IGNORECASE)
