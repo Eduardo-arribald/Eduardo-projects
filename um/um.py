f4e453ac840a9171b9c48e7c4b772e1904bb31c5
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-03-21 13:21:59 +0000

    Tue, Mar 21, 2023, 7:21 AM -06:00

---
+++ um/um.py
@@ -0,0 +1,15 @@
+import re
+import sys
+
+
+def main():
+    #print(count(input("Text: ")))
+    x = "Hola"
+    print(count(x))
+
+def count(s):
+    ...
+
+
+if __name__ == "__main__":
+    main()
