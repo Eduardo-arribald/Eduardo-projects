07f0d0591aa42a652bf6d74261e75b1b36a6d0e3
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-15 19:51:57 +0000

    Sat, Apr 15, 2023, 2:51 PM -05:00

--- jar/jar.py
+++ jar/jar.py
@@ -13,13 +13,15 @@ class Jar:
     def deposit(self, n):
         if n + self.size > self.capacity:
             raise ValueError
-        self._size += n
+        else:
+            self._size = self._size + n
 
 
     def withdraw(self, n): #withdraw = retirar
         if n > self._size:
             raise ValueError
-        self._size -= n
+        else:
+            self._size = self._size - n
 
     @property #All property decorators must have a setter.
     def capacity(self):
@@ -28,6 +30,7 @@ class Jar:
     @capacity.setter
     def capacity(self):
         return self._capacity
+
 
     @property
     def size(self):
