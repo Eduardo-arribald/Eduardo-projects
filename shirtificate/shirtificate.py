2565e5add5fa5df821a019be52d6e374aedf31c3
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-18 17:33:20 +0000

    Tue, Apr 18, 2023, 12:33 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,5 +3,5 @@ from fpdf import FPDF
 
 pdf = FPDF(orientation = "Portrait", format = "A4")
 pdf.add_page()
-pdf.image("shirtificate.png", x = 20, y = 60)
+pdf.image("shirtificate.png")
 pdf.output("shirtificate_1.pdf")
