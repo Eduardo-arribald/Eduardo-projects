412b5af8f90bb7559380fd602e7595523f224fa1
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-18 17:45:04 +0000

    Tue, Apr 18, 2023, 12:45 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,6 +3,8 @@ from fpdf import FPDF
 
 pdf = FPDF(orientation = "Portrait", format = "A4")
 pdf.add_page()
+#I first write the header of the image: "CS50 Shirtificate".
+#for do this, I firstly have to define the font. 
 pdf.set_font('helvetica', 'B', 16)
 pdf.image("shirtificate.png")
 pdf.output("shirtificate_1.pdf")
