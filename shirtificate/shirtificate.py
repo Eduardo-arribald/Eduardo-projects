60633281c1c4581a40a9f9ec25f0e6819e5f6d2d
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-18 18:33:21 +0000

    Tue, Apr 18, 2023, 1:33 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,20 +6,21 @@ class PDF(FPDF):
         #self.set_margin(0)
         #Putting the shirt.
         #The size is not ready.
-        self.image("shirtificate.png", x = "C", w = pdf.epw/2)
+        self.image("shirtificate.png", x = "C", w = pdf.epw*(1.5/2))
         #
-        self.set_font("Times", "", 10)
+        self.set_font("Times", "", 40)
+        self.set_y(20)
         #Centering the text "CS50 Shirtificate".
-        self.cell(80)
+        #self.cell(80)
         #Printing the title
-        self.cell(50, 10, "CS50 Shirtificate", align="C")
+        self.cell(0, 10, "CS50 Shirtificate", align="C")
 
 
     def shirtname(self, shirtname):
         # Position cursor at 1.5 cm from bottom:
-        self.set_y(50)
+        self.set_y(100)
         # Setting font: helvetica italic 8
-        self.set_font("Times", "", 50)
+        self.set_font("Times", "", 30)
         # Printing page number:
         self.cell(0, 10, f"{shirtname} took CS50", align="C")
 
