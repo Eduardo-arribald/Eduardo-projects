ec6d9728efb77e5de0c90d40820853fb435532cb
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-22 23:36:07 +0000

    Sat, Apr 22, 2023, 6:36 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -21,9 +21,9 @@ def check_command_line_arg():
         sys.exit("Too many command-line arguments")
     file1 = splitext(sys.argv[1])
     file2 = splitext(sys.argv[2])
-    if check_extension(file1[1]) == False:
+    if check_extensions(file1[1]) == False:
         sys.exit("Invalid input")
-    if check_extension(file2[1]) == False:
+    if check_extensions(file2[1]) == False:
         sys.exit("Invalid output")
     if file1[1].lower() != file2[1].lower():
         sys.exit("Input and output have different extensions")
