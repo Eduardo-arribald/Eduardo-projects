3418d4c49e45af3bcc514b0be13574071b3fe486
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-22 23:19:28 +0000

    Sat, Apr 22, 2023, 6:19 PM -05:00

--- shirt/shirt_2.py
+++ shirt/shirt_2.py
@@ -6,3 +6,6 @@ def main():
     check_command_line_arg()
     try:
         imagefile = Image.open(sys.argv[1])
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+    shirtfile = 
