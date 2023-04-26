6d3de72d89039844e4bda45dbe6feed9af3f43dd
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-26 19:30:29 +0000

    Wed, Apr 26, 2023, 2:30 PM -05:00

--- final/final_project.py
+++ final/final_project.py
@@ -1,9 +1,12 @@
 
 
 #    What will your software do? What features will it have? How will it be executed?
-#   I will make a program for 
+#   I will make a program for countability administration of startups, in a simple and comprehensible way.
+#   Features:
+#    Presentation of assets, passives, and capital(elements of a general balance) and information of a sales statement.
+#    This information will be easily watchable in an excel file.
 #    What new skills will you need to acquire? What topics will you need to research?
-#
+#    Knowledge of all the information under a basic balance sheet and a sales statement. 
 #    In the world of software, most everything takes longer to implement than you expect. And so it’s not uncommon to accomplish less in a fixed amount of time than you hope. What might you consider to be a good outcome for your project? A better outcome? The best outcome?
 
 
