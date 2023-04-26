5b9d4c08ee7543970063220db8e838c537c9868e
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-26 19:07:14 +0000

    Wed, Apr 26, 2023, 2:07 PM -05:00

---
+++ final/final_project.py
@@ -0,0 +1,17 @@
+
+
+
+def main():
+    print("Here is the begining of the end.")
+
+
+#These three functions will have to be tested with pytest.
+
+def function_1():
+    print("idk what this is going to do.")
+
+def function_2():
+    print("idk what this is going to do x2.")
+
+def function_3():
+    print("idk what this is going to do x3.")
