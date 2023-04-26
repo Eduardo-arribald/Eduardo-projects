2ee786bd81ad2087896fe1809331a937604dcb7c
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-04-26 19:26:03 +0000

    Wed, Apr 26, 2023, 2:26 PM -05:00

--- final/final_project.py
+++ final/final_project.py
@@ -1,7 +1,7 @@
 
 
 #    What will your software do? What features will it have? How will it be executed?
-# 
+#   I will make a program for 
 #    What new skills will you need to acquire? What topics will you need to research?
 #
 #    In the world of software, most everything takes longer to implement than you expect. And so it’s not uncommon to accomplish less in a fixed amount of time than you hope. What might you consider to be a good outcome for your project? A better outcome? The best outcome?
