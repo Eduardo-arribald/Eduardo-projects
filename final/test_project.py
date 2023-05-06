11e53880573d73b339347173fad05d33bce4ccbc
Eduardo-arribald <122075891+Eduardo-arribald@users.noreply.github.com>
2023-05-06 00:18:31 +0000

    Fri, May 5, 2023, 7:18 PM -05:00

--- final/test_project.py
+++ final/test_project.py
@@ -13,18 +13,18 @@ Assets_list = [
     "Accounts receivable", "Stock", "Other circulating assets",
     "Fixed and deferred assets", "Accounts receivable LT", "Investments LT",
     "Buildings and land", "Machinery", "Equipment", "Accumulated amortization and depretiation",
-    "Deferred assets", None, "Total assets", "Date:", f"Last modification\ndate: {actual_date}"]
+    "Deferred assets", None, "Total assets", "Date:", f"Last modification\ndate: "]
 balance_sheet = {
     "Assets": Assets_list,
     "Amount_1": [f"$ {0}"]*15 +["",f"$ {0}"]+[""]*2,
     "Liabilities and Equity": liabilities_list + equity_list + [""]*2,
     "Amount_2": [f"$ {0}"]*10 + [""]+ [f"$ {0}"]*6 + [""]*2
     }
-    balance_sheet["Amount_2"][11] = ""
+balance_sheet["Amount_2"][11] = ""
 
 def test_search_for_index():
-    assert search_for_index(balance_sheet, "Cash") == "Assets", 0
-    assert search_for_index(balance_sheet, "Machinery") == "Assets", 11
+    assert search_for_index(balance_sheet, "Cash") == ("Assets", 1)
+    assert search_for_index(balance_sheet, "Machinery") == ("Assets", 11)
 
 
 def test_add_money_format():
@@ -37,6 +37,6 @@ def test_add_money_format():
 def test_quit_money_format():
     assert quit_money_format("$ 99") == 99
     assert quit_money_format("$ 51204") == 51204
-    assert quit_money_format("$ ")
+    assert quit_money_format("$ 595") == 595
     with pytest.raises(ValueError):
         quit_money_format("$ asdad")
